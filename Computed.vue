a3eb20cde528d5f6b9013b10e8b10da47a934d54
kabircse2018 <kabircse2018@gmail.com>
2023-10-14 21:26:25 +0600

    first commit

---
+++ Computed.vue
@@ -0,0 +1,34 @@
+<script>
+import { computed } from 'vue'
+
+const user = ref({
+  name: 'John Doe',
+  age: 30,
+})
+
+const birthYear = computed(() => {
+  return new Date().getFullYear() - user.value.age
+})
+
+const canVote = computed(() => {
+    if (user.value.age >= 18)
+    {
+        console.log('eligible for voting')
+    }
+    else
+    {
+        console.log('not eligible for voting')
+    }
+})
+
+</script>
+
+<template>
+  <div>
+    <h1>User Profile</h1>
+    <p>Name: {{ user.name }}</p>
+    <p>Age: {{ user.age }}</p>
+    <p>Birth Year: {{ birthYear }}</p>
+    <p>Can Vote: {{ canVote }}</p>
+  </div>
+</template>
---
+++ Watchers.vue
@@ -0,0 +1,36 @@
+<script>
+import {ref, watch } from 'vue'
+
+export const user = ref({
+  name: 'John Doe',
+  age: 30,
+})
+
+const birthYear = watch(() => {
+  return new Date().getFullYear() - user.value.age
+})
+
+const canVote = watch(() => {
+  if (user.value.age >= 18)
+  {
+      console.log('eligible for voting')
+  }
+  else
+  {
+      console.log('not eligible for voting')
+  }
+})
+
+
+</script>
+
+<template>
+  <div>
+    <h1>User Profile</h1>
+    <p>Name: {{ user.name }}</p>
+    <p>Age: {{ user.age }}</p>
+    <p>Birth Year: {{ birthYear }}</p>
+    <p>Can Vote: {{ canVote }}</p>
+  </div>
+</template>
+
